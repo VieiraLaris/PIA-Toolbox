d81a304b97966eeac10a530a84ef1a26649bef57
VieiraLaris <lalasarandi@gmail.com>
2026-09-07 18:01:36 -0300

    feat: adiciona lógica de código de identificação e contador de MFs na classe input

--- src/core/PIAInput.m
+++ src/core/PIAInput.m
@@ -1,5 +1,9 @@
 classdef PIAInput
     % PIAInput represents an input variable of a PIA fuzzy system
+    properties (SetAccess = private)
+        ID string
+        MFCounter = 0
+    end
     
     properties
         Name string
@@ -8,14 +12,17 @@ classdef PIAInput
     end
     
     methods
-        function obj = PIAInput(name, range)
+        function obj = PIAInput(id, name, range)
+            obj.ID = id;
             obj.Name = name;
             obj.Range = range;
         end
         
         % Função para adicionar uma MF ao input
         function obj = addMF(obj, name, type, parameters)
-            mf = PIAMembershipFunction(name, type, parameters);
+            obj.MFCounter = obj.MFCounter + 1;
+            id = "mf_" + string(obj.MFCounter);
+            mf = PIAMembershipFunction(id, name, type, parameters);
             obj.MembershipFunctions(end + 1) = mf; % Adiciona uma nova mf na posição seguinte
         end
     end
