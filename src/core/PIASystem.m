c6eebdf485212f5d5942ef87cc834c1a6109bc7b
VieiraLaris <lalasarandi@gmail.com>
2026-09-07 18:05:40 -0300

    feat: adiciona métodos de criação e adição de inputs, outputs e funções de pertinências na classe system

--- src/core/PIASystem.m
+++ src/core/PIASystem.m
@@ -4,20 +4,49 @@ classdef PIASystem
     % As 'properties' da classe representam seus atributos
     properties
         Name
-        Inputs
-        Outputs
+        Inputs PIAInput = PIAInput.empty
+        Outputs PIAOutput = PIAOutput.empty
         Rules
         Parameters
+    end
+
+    % Contador interno para gerar IDs dos inputs
+    properties (Access = private)
+        InputCounter = 0
+        OutputCounter = 0
     end
 
     methods
         % O construtor
         function obj = PIASystem(name)
             obj.Name = name;
-            obj.Inputs = struct([]); % struct([]) -> seria uma lista de structs (no caso uma lista de structs do tipo Input)
-            obj.Outputs = struct([]);
-            obj.Rules = struct([]);
-            obj.Parameters = struct(); % struct() -> apenas uma struct do tipo Parameters
+        end
+
+        % Método responsável por criar e adicionar um Input ao sistema
+        function obj = addInput(obj, name, range)
+            obj.InputCounter = obj.InputCounter + 1;
+            id = "input_" + string(obj.InputCounter);
+            input = PIAInput(id, name, range);
+            obj.Inputs(end + 1) = input;
+        end
+
+        % Método responsável por criar e adicionar um Output ao sistema
+        function obj = addOutput(obj, name, range)
+            obj.OutputCounter = obj.OutputCounter + 1;
+            id = "output_" + string(obj.OutputCounter);
+            output = PIAOutput(id, name, range);
+            obj.Outputs(end + 1) = output;
+        end
+
+        % Adiciona uma Membership Function a um Input/Output existente
+        function obj = addMF(obj, variableType, variableIndex, type, parameters, name)
+            if variableType == "input"
+                obj.Inputs(variableIndex) = ...
+                    obj.Inputs(variableIndex).addMF(name, type, parameters);
+            elseif variableType == "output"
+                obj.Outputs(variableIndex) = ...
+                    obj.Outputs(variableIndex).addMF(name, type, parameters);
+            end
         end
     end
 end
