f4f79323191776743e59656bdccb42522e404e6f
VieiraLaris <lalasarandi@gmail.com>
2026-09-13 22:34:11 -0300

    feat: adiciona função de setup

---
+++ src/setupPIA.m
@@ -0,0 +1,10 @@
+function setupPIA()
+
+root = fileparts(mfilename('fullpath'));
+
+addpath(fullfile(root, 'core'));
+addpath(fullfile(root, 'engine'));
+addpath(fullfile(root, 'functions'));
+addpath(fullfile(root, 'tests'));
+
+end
